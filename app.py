5f74d4c35b18009260266f8b3fcb7831626d087d
vikash kumar <vkvikashkumar987@gmail.com>
2022-09-25 18:03:50 +0530

    changes of port

--- app.py
+++ app.py
@@ -39,5 +39,4 @@ def predict():
      return str(prediction[0])
 
 if __name__=='__main__':
-     port = int(os.environ.get('PORT', 5000))
-     app.run(debug=True, host='0.0.0.0', port=port)
+     app.run(debug=True)
