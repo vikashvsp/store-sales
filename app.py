4d888838bcbcc485f626e66caea191251ff44e20
kushagratandon12 <kushagra.tandon@yahoo.com>
2022-10-02 13:00:06 +0530

    update: dockerfile #1

--- app.py
+++ app.py
@@ -1,33 +1,28 @@
 import logging
 import pandas as pd
 
-from fileinput import filename
 from flask import Flask,request,render_template
 
-from utils.utils import read_request_data
+from utils.utils import read_request_data , read_csv_file
 from model.inference import predict_model
 
 app=Flask(__name__)
-
-def read_csv_file(filename):
-     try:
-          logging.info(f'Reading CSV File {filename}')
-          return pd.read_csv(filename)
-     except Exception as err:
-          logging.error(f'Failed in loding csv file {filename}')
-
-logging.basicConfig(format=(),level=logging.INFO)
+logging.basicConfig(format='%(asctime)s-%(levelname)s- %(message)s ', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 
 @app.route('/')
 def home():
-     data = read_csv_file('./cleaned_data.csv')
+     """
+     #TODO: Explain what this function does 
+     """
+     sales_data = read_csv_file('cleaned_data.csv')
+     logging.debug(f'CSV Data Loaded {sales_data}')
      
-     Item_Fat_Content=sorted(data['Item_Fat_Content'].unique())
-     Item_Type=sorted(data['Item_Type'].unique())
-     Outlet_Size=sorted(data['Outlet_Size'].unique())
-     Outlet_Location_Type=sorted(data['Outlet_Location_Type'].unique())
-     Outlet_Type=sorted(data['Outlet_Type'].unique())
+     Item_Fat_Content=sorted(sales_data['Item_Fat_Content'].unique())
+     Item_Type=sorted(sales_data['Item_Type'].unique())
+     Outlet_Size=sorted(sales_data['Outlet_Size'].unique())
+     Outlet_Location_Type=sorted(sales_data['Outlet_Location_Type'].unique())
+     Outlet_Type=sorted(sales_data['Outlet_Type'].unique())
 
      return render_template("index.html", Item_Fat_Content= Item_Fat_Content, Item_Type= Item_Type, Outlet_Size=Outlet_Size,
      Outlet_Location_Type= Outlet_Location_Type, Outlet_Type=Outlet_Type)
@@ -40,7 +35,9 @@ def predict():
      """
      sales_data = read_request_data(request=request)
      prediction = predict_model(sales_data)
+     logging.info(f'Predictions Made By Model {prediction}')
+
      return str(prediction[0])
 
 if __name__=='__main__':
-     app.run(debug=True)
+     app.run(debug=False)
--- utils/utils.py
+++ utils/utils.py
@@ -1,17 +1,31 @@
+from cmath import log
 from flask import request
+import logging
+import pandas as pd
+import sys
+
+def read_csv_file(filename):
+    try:
+        logging.info(f'Reading CSV File {filename}')
+        return pd.read_csv("./data/" + filename)
+    except Exception as err:
+        logging.error(f'Failed in loading csv file {filename} error: {err}')
 
 def read_request_data(request):
+    try:
+        Item_Weight=float(request.form.get('Item_Weight'))
+        Item_Fat_Content = request.form.get('Item_Fat_Content')
+        Item_Visibility = request.form.get('Item_Visibility')
+        Item_Type = request.form.get('Item_Type')
+        Item_MRP = request.form.get('Item_MRP')
+        Outlet_Size = request.form.get('Outlet_Size')
+        Outlet_Location_Type = request.form.get('Outlet_Location_Type')
+        Outlet_Type = request.form.get('Outlet_Type')
+        Outlet_Establishment_Year = request.form.get('Outlet_Establishment_Year')
 
-    Item_Weight=float(request.form.get('Item_Weight'))
-    Item_Fat_Content = request.form.get('Item_Fat_Content')
-    Item_Visibility = request.form.get('Item_Visibility')
-    Item_Type = request.form.get('Item_Type')
-    Item_MRP = request.form.get('Item_MRP')
-    Outlet_Size = request.form.get('Outlet_Size')
-    Outlet_Location_Type = request.form.get('Outlet_Location_Type')
-    Outlet_Type = request.form.get('Outlet_Type')
-    Outlet_Establishment_Year = request.form.get('Outlet_Establishment_Year')
+        sales_data = [Item_Weight, Item_Fat_Content, Item_Visibility, Item_Type,Item_MRP, Outlet_Size, Outlet_Location_Type, Outlet_Type, Outlet_Establishment_Year]
+        return sales_data
 
-    sales_data = [Item_Weight, Item_Fat_Content, Item_Visibility, Item_Type,Item_MRP, Outlet_Size, Outlet_Location_Type, Outlet_Type, Outlet_Establishment_Year]
-
-    return sales_data
+    except Exception as err:
+        logging.error(f'Error in processing the request: {err}')
+        return "Error"
