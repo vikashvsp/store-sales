ff54332a6593ac312b1c2f45f6240ecc4f0e3c83
kushagratandon12 <kushagra.tandon@yahoo.com>
2022-10-02 13:05:38 +0530

    update: cleaning

--- app.py
+++ app.py
@@ -1,43 +1,44 @@
 import logging
-import pandas as pd
 
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 
-from utils.utils import read_request_data , read_csv_file
+from utils.utils import read_request_data, read_csv_file
 from model.inference import predict_model
 
-app=Flask(__name__)
+app = Flask(__name__)
 logging.basicConfig(format='%(asctime)s-%(levelname)s- %(message)s ', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 
 @app.route('/')
 def home():
-     """
-     #TODO: Explain what this function does 
-     """
-     sales_data = read_csv_file('cleaned_data.csv')
-     logging.debug(f'CSV Data Loaded {sales_data}')
-     
-     Item_Fat_Content=sorted(sales_data['Item_Fat_Content'].unique())
-     Item_Type=sorted(sales_data['Item_Type'].unique())
-     Outlet_Size=sorted(sales_data['Outlet_Size'].unique())
-     Outlet_Location_Type=sorted(sales_data['Outlet_Location_Type'].unique())
-     Outlet_Type=sorted(sales_data['Outlet_Type'].unique())
+    """
+    #TODO: Explain what this function does
+    """
+    sales_data = read_csv_file('cleaned_data.csv')
+    logging.debug(f'CSV Data Loaded {sales_data}')
 
-     return render_template("index.html", Item_Fat_Content= Item_Fat_Content, Item_Type= Item_Type, Outlet_Size=Outlet_Size,
-     Outlet_Location_Type= Outlet_Location_Type, Outlet_Type=Outlet_Type)
+    Item_Fat_Content = sorted(sales_data['Item_Fat_Content'].unique())
+    Item_Type = sorted(sales_data['Item_Type'].unique())
+    Outlet_Size = sorted(sales_data['Outlet_Size'].unique())
+    Outlet_Location_Type = sorted(sales_data['Outlet_Location_Type'].unique())
+    Outlet_Type = sorted(sales_data['Outlet_Type'].unique())
+
+    return render_template("index.html", Item_Fat_Content=Item_Fat_Content, Item_Type=Item_Type,
+                           Outlet_Size=Outlet_Size,
+                           Outlet_Location_Type=Outlet_Location_Type, Outlet_Type=Outlet_Type)
 
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-     """
-     This function returns the predicted data back to the webpage. 
-     """
-     sales_data = read_request_data(request=request)
-     prediction = predict_model(sales_data)
-     logging.info(f'Predictions Made By Model {prediction}')
+    """
+    This function returns the predicted data back to the webpage.
+    """
+    sales_data = read_request_data(request=request)
+    prediction = predict_model(sales_data)
+    logging.info(f'Predictions Made By Model {prediction}')
 
-     return str(prediction[0])
+    return str(prediction[0])
 
-if __name__=='__main__':
-     app.run(debug=False)
+
+if __name__ == '__main__':
+    app.run(debug=False)
--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,7 @@
-from cmath import log
 from flask import request
 import logging
 import pandas as pd
-import sys
+
 
 def read_csv_file(filename):
     try:
@@ -11,9 +10,10 @@ def read_csv_file(filename):
     except Exception as err:
         logging.error(f'Failed in loading csv file {filename} error: {err}')
 
+
 def read_request_data(request):
     try:
-        Item_Weight=float(request.form.get('Item_Weight'))
+        Item_Weight = float(request.form.get('Item_Weight'))
         Item_Fat_Content = request.form.get('Item_Fat_Content')
         Item_Visibility = request.form.get('Item_Visibility')
         Item_Type = request.form.get('Item_Type')
@@ -23,7 +23,8 @@ def read_request_data(request):
         Outlet_Type = request.form.get('Outlet_Type')
         Outlet_Establishment_Year = request.form.get('Outlet_Establishment_Year')
 
-        sales_data = [Item_Weight, Item_Fat_Content, Item_Visibility, Item_Type,Item_MRP, Outlet_Size, Outlet_Location_Type, Outlet_Type, Outlet_Establishment_Year]
+        sales_data = [Item_Weight, Item_Fat_Content, Item_Visibility, Item_Type, Item_MRP, Outlet_Size,
+                      Outlet_Location_Type, Outlet_Type, Outlet_Establishment_Year]
         return sales_data
 
     except Exception as err:
